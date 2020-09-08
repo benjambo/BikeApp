98078f1fc2c1cd7995666688ddf283bd27510f81
Benjamin Bowo <ben.bowo@gmail.com>
2020-09-08 17:15:36 +0300

    Updates

---
+++ precache-manifest.2327803092b968e51f3c1a48f83de34d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50372d0db44137b490d08d7ff6fe1335",
+    "url": "/bikeapp/index.html"
+  },
+  {
+    "revision": "2bd3daefcbf118913115",
+    "url": "/bikeapp/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "36061766a9d06b97b91c",
+    "url": "/bikeapp/static/css/main.a3e73d7e.chunk.css"
+  },
+  {
+    "revision": "2bd3daefcbf118913115",
+    "url": "/bikeapp/static/js/2.6db099c3.chunk.js"
+  },
+  {
+    "revision": "c8f4238afe9b5d33761aa288ebe870db",
+    "url": "/bikeapp/static/js/2.6db099c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36061766a9d06b97b91c",
+    "url": "/bikeapp/static/js/main.9f0dee23.chunk.js"
+  },
+  {
+    "revision": "17d8e7df7b6de33e0e57",
+    "url": "/bikeapp/static/js/runtime-main.0b91fd37.js"
+  }
+]);
